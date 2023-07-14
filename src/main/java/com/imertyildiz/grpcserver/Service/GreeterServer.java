58bc4bb0e6e289871cadf3cf79a2464eae1b98a9
imert* <mert.yildiz@kuartis.com>
2023-07-14 15:39:40 +0300

    change system out to logger

--- src/main/java/com/imertyildiz/grpcserver/Service/GreeterServer.java
+++ src/main/java/com/imertyildiz/grpcserver/Service/GreeterServer.java
@@ -1,5 +1,8 @@
 package com.imertyildiz.grpcserver.Service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.imertyildiz.grpcproto.HelloWorldRequest;
 import com.imertyildiz.grpcproto.HelloWorldResponse;
 import com.imertyildiz.grpcproto.HelloWorldServiceGrpc;
@@ -10,12 +13,13 @@ import net.devh.boot.grpc.server.service.GrpcService;
 
 @GrpcService
 public class GreeterServer extends HelloWorldServiceGrpc.HelloWorldServiceImplBase {
+    private static final Logger logger = LoggerFactory.getLogger(GreeterServer.class);
 
     @Override
     public void helloWorld(HelloWorldRequest request, StreamObserver<HelloWorldResponse> responseObserver) {
         HelloWorldResponse setResponseMessage = HelloWorldResponse.newBuilder()
                 .setResponseMessage("Hello " + request.getClientName() + " !!!").build();
-        System.out.println(request.getClientName() + " sent a message: " + request.getRequestMessage());
+        logger.info(String.format("%1s sent a message: %1s", request.getClientName(),request.getRequestMessage()));
         responseObserver.onNext(setResponseMessage);
         responseObserver.onCompleted();
     }
